ee8c69a1692c42f2ecd2de2efa853f997cfbbd91
Anayely <anayelialmonte@gmail.com>
2024-10-08 11:00:56 -0400

    up to question 5

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 //Write your solutions in this file, don't forget to test your functions.
+
+    const countUpFromOne = (n) => {
+        for(let i = 1; i <= n; i++) {
+          console.log(i);
+        }
+      };
+
+      countUpFromOne(10);
+
+      
+        const countEveryOdd = (n) => {
+        for(i = 1; i <= n; i++) {
+          if(i % 2 !== 0) {
+            console.log(i);
+          }
+        }
+    };
+    countEveryOdd(10);
+
+
+    const isNegative = (num) => {
+       return num < 0; 
+    };
+           
+
+    console.log(isNegative(3));
+
+    const betweenFiveAndTwenty = (num) => {
+        return num >= 5 && num <= 20;
+    };
+    console.log(betweenFiveAndTwenty(12));
+
+
+
+
